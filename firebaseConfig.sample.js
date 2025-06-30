09c698ac45cf431c590a1cd7b0efc083f616d9f8
hidden987 <overnight@OVERNIGHT-WINDOWS.localdomain>
2025-06-30 20:31:37 +0900

    Feat: 방명록 기능 추가 및 db 연동

---
+++ firebaseConfig.sample.js
@@ -0,0 +1,9 @@
+// firebaseConfig.js
+export const firebaseConfig = {
+    apiKey: "",
+    authDomain: "index-us-db.firebaseapp.com",
+    projectId: "index-us-db",
+    storageBucket: "index-us-db.firebasestorage.app",
+    messagingSenderId: "",
+    appId: ":::"
+};
